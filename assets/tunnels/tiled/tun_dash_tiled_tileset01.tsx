3b02645a143b41254d8da599d58c7c775a5fc12a
Kent Dahl <kentda@pvv.org>
2024-05-10 18:30:49 +0200

    Tiled: original tileset.

---
+++ assets/tunnels/tiled/tun_dash_tiled_tileset01.tsx
@@ -0,0 +1,157 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="sprites" tilewidth="32" tileheight="32" tilecount="90" columns="10">
+ <editorsettings>
+  <export target="sprites-tileset.json" format="json"/>
+ </editorsettings>
+ <image source="../../images/sprites.png" width="320" height="288"/>
+ <tile id="9">
+  <properties>
+   <property name="Name" value="splat"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="Name" value="spell-lightning"/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="Name" value="spell-ball"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="Name" value="spell-cube"/>
+  </properties>
+ </tile>
+ <tile id="20">
+  <properties>
+   <property name="Name" value="chest"/>
+  </properties>
+ </tile>
+ <tile id="21">
+  <properties>
+   <property name="Name" value="potion-heal"/>
+  </properties>
+ </tile>
+ <tile id="22">
+  <properties>
+   <property name="Name" value="potion-mana"/>
+  </properties>
+ </tile>
+ <tile id="23">
+  <properties>
+   <property name="Name" value="moneybag"/>
+  </properties>
+ </tile>
+ <tile id="24">
+  <properties>
+   <property name="Name" value="orb"/>
+  </properties>
+ </tile>
+ <tile id="30">
+  <properties>
+   <property name="Name" value="key-yellow"/>
+  </properties>
+ </tile>
+ <tile id="39">
+  <properties>
+   <property name="Name" value="exit-portal"/>
+  </properties>
+ </tile>
+ <tile id="40">
+  <properties>
+   <property name="Name" value="meanie"/>
+  </properties>
+ </tile>
+ <tile id="41">
+  <properties>
+   <property name="Name" value="slimer"/>
+  </properties>
+ </tile>
+ <tile id="42">
+  <properties>
+   <property name="Name" value="devil"/>
+  </properties>
+ </tile>
+ <tile id="43">
+  <properties>
+   <property name="Name" value="vore"/>
+  </properties>
+ </tile>
+ <tile id="44">
+  <properties>
+   <property name="Name" value="demon"/>
+  </properties>
+ </tile>
+ <tile id="45">
+  <properties>
+   <property name="Name" value="spider"/>
+  </properties>
+ </tile>
+ <tile id="46">
+  <properties>
+   <property name="Name" value="grinder"/>
+  </properties>
+ </tile>
+ <tile id="47">
+  <properties>
+   <property name="Name" value="snotty"/>
+  </properties>
+ </tile>
+ <tile id="48">
+  <properties>
+   <property name="Name" value="deadhead"/>
+  </properties>
+ </tile>
+ <tile id="49">
+  <properties>
+   <property name="Name" value="cultist"/>
+  </properties>
+ </tile>
+ <tile id="50">
+  <properties>
+   <property name="Name" value="snake"/>
+  </properties>
+ </tile>
+ <tile id="51">
+  <properties>
+   <property name="Name" value="octopus"/>
+  </properties>
+ </tile>
+ <tile id="52">
+  <properties>
+   <property name="Name" value="ogre"/>
+  </properties>
+ </tile>
+ <tile id="53">
+  <properties>
+   <property name="Name" value="orc"/>
+  </properties>
+ </tile>
+ <tile id="54">
+  <properties>
+   <property name="Name" value="wraith"/>
+  </properties>
+ </tile>
+ <tile id="55">
+  <properties>
+   <property name="Name" value="skull"/>
+  </properties>
+ </tile>
+ <tile id="56">
+  <properties>
+   <property name="Name" value="knight"/>
+  </properties>
+ </tile>
+ <tile id="57">
+  <properties>
+   <property name="Name" value="mage"/>
+  </properties>
+ </tile>
+ <tile id="63">
+  <properties>
+   <property name="Name" value="floor-portal"/>
+  </properties>
+ </tile>
+</tileset>
